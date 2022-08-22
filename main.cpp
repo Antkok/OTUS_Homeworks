9c4d38b4d4c9e0f15efe7fa62005dc2cdff54ab6
Antkok <Antkok@yandex.ru>
2022-08-22 23:47:08 +0500

    add vector and list instead of containers

--- main.cpp
+++ main.cpp
@@ -16,13 +16,14 @@ void print_ip(T ip)
 int main()
 {
     //std::cout << "Print IP version " << version() << std::endl;
-    print_ip(char{-1});
-    print_ip(short{0});
-    print_ip(int{2130706433});
-    print_ip<long long>(8875824491850138409);
-    print_ip(std::string{"trololo"});
-    print_ip(std::vector{0, 1, 2, 3});
-    print_ip(std::list{10, 20, 30, 40});
-    print_ip(std::make_tuple(0, 1, 2, 3));
+    print_ip(int8_t{ -1 }); // 255
+    print_ip(int16_t{ 0 }); // 0.0
+    print_ip(int32_t{ 2130706433 }); // 127.0.0.1
+    print_ip(int64_t{ 8875824491850138409 });// 123.45.67.89.101.112.131.41
+    print_ip(std::string{ "Hello, World!"}); // Hello, World!
+    print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400
+    print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100
+    print_ip(std::make_tuple(123, 456, 789, 0)); // 123.456.789.0
+      
     return 0;
 }
